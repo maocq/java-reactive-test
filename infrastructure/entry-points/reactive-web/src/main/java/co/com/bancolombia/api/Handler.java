03e20a050b52ffac510171841e1d633c0d468c89
maocq <johncarm@bancolombia.com.co>
2023-07-10 07:55:30 -0500

    Entry point

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,28 +1,32 @@
 package co.com.bancolombia.api;
 
+import co.com.bancolombia.api.dto.request.RegisterAccountRequest;
+import co.com.bancolombia.model.exceptions.BusinessException;
+import co.com.bancolombia.usecase.registeraccount.RegisterAccountUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import static co.com.bancolombia.model.exceptions.message.BusinessErrorMessage.INVALID_REQUEST;
+
 @Component
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
+
+    private final RegisterAccountUseCase registerAccountUseCase;
+
     public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+        return serverRequest.bodyToMono(RegisterAccountRequest.class)
+                .switchIfEmpty(Mono.error(() -> new BusinessException(INVALID_REQUEST)))
+                .flatMap(request ->
+                        registerAccountUseCase.register(request.getId(), request.getName(), request.getStatusId()))
+                .flatMap(account -> ServerResponse.ok().bodyValue(account));
     }
 
     public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
         // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
-
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
         return ServerResponse.ok().bodyValue("");
     }
 }
---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/request/RegisterAccountRequest.java
@@ -0,0 +1,17 @@
+package co.com.bancolombia.api.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class RegisterAccountRequest {
+
+    private long id;
+    private String name;
+    private String statusId;
+}
